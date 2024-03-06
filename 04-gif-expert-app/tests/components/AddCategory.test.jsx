43fe8dcd1e324351c6f69fc84e4ca3e0d253aaf7
braian <brasanluc123@gmail.com>
2024-03-06 20:15:01 -0300

    I use jest.fn() to create a mock of the onNewCategory function, which allows you to check if it has been called and with what arguments.

--- 04-gif-expert-app/tests/components/AddCategory.test.jsx
+++ 04-gif-expert-app/tests/components/AddCategory.test.jsx
@@ -10,12 +10,25 @@ describe("Pruebas en <AddCatefory/>", () => {
 
   test("debe de llamar onNewCategory si el input tiene un valor ", () => {
     const inputValue = "Saitama";
-    render(<AddCategory oneNewCategory={() => {}} />);
+    const onNewCategory = jest.fn();
+    render(<AddCategory oneNewCategory={onNewCategory} />);
     const input = screen.getByRole("textbox");
     const form = screen.getByRole("form");
     fireEvent.input(input, { target: { value: inputValue } });
     fireEvent.submit(form);
     // screen.debug();
     expect(input.value).toBe("");
+    expect(onNewCategory).toHaveBeenCalled();
+    expect(onNewCategory).toHaveBeenCalledTimes(1);
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+  });
+
+  test("no debe de llamar el onNewCategory si el input esta vacio ", () => {
+    const onNewCategory = jest.fn();
+    render(<AddCategory oneNewCategory={onNewCategory} />);
+    fireEvent.submit(form);
+
+    expect(onNewCategory).toHaveBeenCalledTimes(0);
+    expect(onNewCategory).not.toHaveBeenCalled();
   });
 });
