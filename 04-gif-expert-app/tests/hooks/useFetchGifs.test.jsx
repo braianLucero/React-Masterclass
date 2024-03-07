165e7663cc9b214cd79b0d4cba08df0e49d824bb
braian <brasanluc123@gmail.com>
2024-03-07 12:16:36 -0300

    Tests in the <useFetch Gif/> hook, where the initial state is returning

---
+++ 04-gif-expert-app/tests/hooks/useFetchGifs.test.jsx
@@ -0,0 +1,9 @@
+import { renderHook } from "@testing-library/react";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+describe("Pruebas en el hook <useFetchGifs/>", () => {
+  test("debe de regresar el estado inicial ", () => {
+    const { result } = renderHook(() => useFetchGifs("One Punch"));
+    const { images, isLoading } = result.current;
+  });
+});
