b486b28f03bd1a60f706cf4907507efbeb45046f
braian <brasanluc123@gmail.com>
2024-03-07 10:10:35 -0300

    GifGrid Mock customHook component testing

--- 04-gif-expert-app/tests/components/GifGrid.test.jsx
+++ 04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -3,6 +3,11 @@ import { GifGrid } from "../../src/components/GifGrid";
 describe("Pruebas en <GifGrid/>", () => {
   const category = "One punch";
   test("debe de mostrar el loading inicialmente ", () => {
-    render(<GifGrid />);
+    render(<GifGrid category={category} />);
+    expect(screen.getByText("cargando.."));
+    expect(screen.getByText(category));
+    screen.debug();
   });
+
+  test("debe de mostrar items cuando se cargan las imagenes useFetch", () => {});
 });
