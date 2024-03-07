90ac982ea0d917b87d5cbbf2752bc1f3e108fcd1
braian <brasanluc123@gmail.com>
2024-03-07 10:33:10 -0300

    use the expect function to verify that two <img> elements are rendered within the GifGrid component by using screen.getAllByRole(img).length and expecting it to be equal to 2.

--- 04-gif-expert-app/tests/components/GifGrid.test.jsx
+++ 04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -1,13 +1,39 @@
 import { render } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+jest.mock("../../src/hooks/useFetchGifs");
 describe("Pruebas en <GifGrid/>", () => {
   const category = "One punch";
   test("debe de mostrar el loading inicialmente ", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
     render(<GifGrid category={category} />);
     expect(screen.getByText("cargando.."));
     expect(screen.getByText(category));
     screen.debug();
   });
 
-  test("debe de mostrar items cuando se cargan las imagenes useFetch", () => {});
+  test("debe de mostrar items cuando se cargan las imagenes useFetch", () => {
+    const gifs = [
+      {
+        id: "ABC",
+        title: "Saitama",
+        url: "https://LocalHost/Saitama.jpg",
+      },
+      {
+        id: "123",
+        title: "Goku",
+        url: "https://LocalHost/Goku.jpg",
+      },
+    ];
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: false,
+    });
+
+    render(<GifGrid category={category} />);
+    expect(screen.getAllByRole("img").length).toBe(2);
+  });
 });
