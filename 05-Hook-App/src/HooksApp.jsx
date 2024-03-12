f93d1f2837f5b889071934a265052b5b4b533ccf
braian <brasanluc123@gmail.com>
2024-03-12 17:31:21 -0300

    make a form with a CustomHook

--- 05-Hook-App/src/HooksApp.jsx
+++ 05-Hook-App/src/HooksApp.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { SimpleForm } from "./02-useEffect/SimpleForm";
+import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+// import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHooks } from "./01-useState/CounterWithCustomHooks";
 
 export const HooksApp = () => {
-  return <SimpleForm />;
+  return <FormWithCustomHook />;
 };
