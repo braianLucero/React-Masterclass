2a13fbceaa5fca10e6c6bb6e00e51fa89d0006a9
braian <brasanluc123@gmail.com>
2024-03-12 11:38:17 -0300

    I defined three functions within useCounter in which I am exporting and I pass as an argument the value in which it is defined and I increment or decrement it 2 by 2

--- 05-Hook-App/src/01-useState/CounterWithCustomHooks.jsx
+++ 05-Hook-App/src/01-useState/CounterWithCustomHooks.jsx
@@ -1,16 +1,22 @@
 import { useCounter } from "../hooks/useCounter";
 
 export const CounterWithCustomHooks = () => {
-  const { counter } = useCounter();
+  const { counter, increment, decrement, reset } = useCounter();
 
   return (
     <>
       <h1>Counter with Hooks : {counter}</h1>
       <hr />
 
-      <button className="btn btn-primary">+1</button>
-      <button className="btn btn-primary">Reset</button>
-      <button className="btn btn-primary">-1</button>
+      <button className="btn btn-primary" onClick={() => increment(2)}>
+        +1
+      </button>
+      <button className="btn btn-primary" onClick={reset}>
+        Reset
+      </button>
+      <button className="btn btn-primary" onClick={() => decrement(2)}>
+        -1
+      </button>
     </>
   );
 };
