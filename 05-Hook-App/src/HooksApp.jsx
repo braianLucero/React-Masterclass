f71b494752f2659d16dbde02f3adf4a69d2117e2
braian <brasanluc123@gmail.com>
2024-03-13 16:19:44 -0300

    first uses of useRef

--- 05-Hook-App/src/HooksApp.jsx
+++ 05-Hook-App/src/HooksApp.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterWithCustomHooks } from "./01-useState/CounterWithCustomHooks";
 
 export const HooksApp = () => {
-  return <MultipleCustomHooks />;
+  return <FocusScreen />;
 };
