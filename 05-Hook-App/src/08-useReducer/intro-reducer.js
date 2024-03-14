04fe1bafbad15399677fbeeaa2de67aba1b578bb
braian <brasanluc123@gmail.com>
2024-03-14 16:05:22 -0300

    developing the bases of a Reducer

--- 05-Hook-App/src/08-useReducer/intro-reducer.js
+++ 05-Hook-App/src/08-useReducer/intro-reducer.js
@@ -1,1 +1,32 @@
-console.log("hola Mundo");
+const inicialState = [
+  {
+    id: 1,
+    todo: "recolectar la piedra del alma ",
+    done: false,
+  },
+];
+
+const todoReducer = (state = inicialState, action = {}) => {
+  if (action.type === "[TODO] add Todo") {
+    return [...state, action.payload];
+  }
+
+  return state;
+};
+
+let todos = todoReducer();
+
+const newTodo = {
+  id: 2,
+  todo: "recolectar la piedra del poder ",
+  done: false,
+};
+
+const addTodoAction = {
+  type: "[TODO] add Todo",
+  payload: newTodo,
+};
+
+todos = todoReducer(todos, addTodoAction);
+
+console.log({ state: todos });
