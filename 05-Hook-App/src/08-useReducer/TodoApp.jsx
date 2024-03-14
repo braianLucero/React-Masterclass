dd5375c437e38512881b0a7b590b305d6429dd41
braian <brasanluc123@gmail.com>
2024-03-14 17:41:55 -0300

    creating the shell of the useReducer All list

--- 05-Hook-App/src/08-useReducer/TodoApp.jsx
+++ 05-Hook-App/src/08-useReducer/TodoApp.jsx
@@ -17,14 +17,39 @@ export const TodoApp = () => {
 
   return (
     <>
-      <h1>TodoApp</h1>
+      <h1>
+        TodoApp: 10, <small>pendientes:2</small>
+      </h1>
       <hr />
-
-      <ul>
-        <li>Item1</li>
-        <li>Item2</li>
-        <li>Item3</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group ">
+            {todos.map((todo) => (
+              <li
+                key={todo.id}
+                className="list-group-item d-flex justify-content-between"
+              >
+                <span className="align-self-center"> Item1</span>
+                <button className="btn btn-danger">Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregando Todo</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="Que hay que hacer ?"
+              className="form-control"
+            />
+            <button type="submit" className="btn btn-outline-primary mt-1">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
