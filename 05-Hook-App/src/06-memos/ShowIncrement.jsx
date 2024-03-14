598cad33d1b3667e03e0ed9a0533d70432370ed9
braian <brasanluc123@gmail.com>
2024-03-14 11:54:33 -0300

    useCallback with arguments

--- 05-Hook-App/src/06-memos/ShowIncrement.jsx
+++ 05-Hook-App/src/06-memos/ShowIncrement.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 export const ShowIncrement = React.memo(({ increment }) => {
-  console.log("Me volvi a generar ");
   return (
     <button
       className="btn btn-primary"
       onClick={() => {
-        increment();
+        increment(5);
       }}
     >
       Incrementar
