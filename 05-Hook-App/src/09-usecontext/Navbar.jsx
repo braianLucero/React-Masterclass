596eefbb2a4c78a42eb519a7538f0653dc06a0e6
braian <brasanluc123@gmail.com>
2024-03-16 15:36:47 -0300

    using navLink tag

--- 05-Hook-App/src/09-usecontext/Navbar.jsx
+++ 05-Hook-App/src/09-usecontext/Navbar.jsx
@@ -1,12 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/about">About</Link>
-    </>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          useContext
+        </Link>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <NavLink
+              className={(isActive) => `nav-link ${isActive} ? 'Active' : ' ' `}
+              to="/"
+            >
+              Home
+            </NavLink>
+
+            <NavLink
+              className={(isActive) => `nav-link ${isActive} ? 'Active' : ' ' `}
+              to="/about"
+            >
+              About
+            </NavLink>
+
+            <NavLink
+              className={(isActive) => `nav-link ${isActive} ? 'Active' : ' ' `}
+              to="/login"
+            >
+              Login
+            </NavLink>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
