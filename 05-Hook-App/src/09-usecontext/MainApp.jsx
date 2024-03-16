c55746368abc3e8dabc5fcb11a3e6740be0552ee
braian <brasanluc123@gmail.com>
2024-03-16 15:13:46 -0300

    using link tags

--- 05-Hook-App/src/09-usecontext/MainApp.jsx
+++ 05-Hook-App/src/09-usecontext/MainApp.jsx
@@ -1,13 +1,17 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, Link } from "react-router-dom";
 import { HomePage } from "./HomePage";
 import { AboutPage } from "./AboutPage";
 import { LoginPage } from "./LoginPage";
+import { Navbar } from "./Navbar";
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp </h1>
+      {/* <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/about">About</Link> */}
+      <Navbar />
       <hr />
-
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
