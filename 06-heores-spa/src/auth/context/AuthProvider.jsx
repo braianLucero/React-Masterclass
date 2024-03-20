40872818f4c9d9525c89c58f9de740a87b941569
braian <brasanluc123@gmail.com>
2024-03-20 18:40:22 -0300

    developing the user login

--- 06-heores-spa/src/auth/context/AuthProvider.jsx
+++ 06-heores-spa/src/auth/context/AuthProvider.jsx
@@ -1,12 +1,26 @@
 import React, { useReducer } from "react";
 import { AuthContext } from "./AuthContext ";
 import { authReducer } from "./authReducer";
+import { types } from "../types/types";
 
 const initialState = {
   logged: false,
 };
 
 export const AuthProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, initialState);
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+  const [authState, dispatch] = useReducer(authReducer, initialState);
+
+  const login = (name = "") => {
+    const action = {
+      type: types.login,
+      payload: {
+        id: "abc",
+        name: name,
+      },
+    };
+    dispatch(action);
+  };
+  return (
+    <AuthContext.Provider value={{ login }}>{children}</AuthContext.Provider>
+  );
 };
--- 06-heores-spa/src/auth/pages/LoginPages.jsx
+++ 06-heores-spa/src/auth/pages/LoginPages.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext ";
 export const LoginPages = () => {
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
   const onLogin = () => {
+    login("Braian lucero");
     navigate("/", {
       replace: true,
     });
