a6958d53749919ea5f919e1fd9c78c7a1e34668f
braian <brasanluc123@gmail.com>
2024-03-20 17:48:03 -0300

    context and reducer of my application

--- 06-heores-spa/src/HeoresApp.jsx
+++ 06-heores-spa/src/HeoresApp.jsx
@@ -1,9 +1,10 @@
+import { AuthProvider } from "./auth";
 import { AppRouter } from "./router/AppRouter";
 
 export const HeoresApp = () => {
   return (
-    <>
+    <AuthProvider>
       <AppRouter />
-    </>
+    </AuthProvider>
   );
 };
