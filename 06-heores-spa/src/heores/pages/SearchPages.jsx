23bf335ce43aad426916ad10ab541b30bc2e0e63
braian <brasanluc123@gmail.com>
2024-03-20 07:59:12 -0300

    designing the search page

--- 06-heores-spa/src/heores/pages/SearchPages.jsx
+++ 06-heores-spa/src/heores/pages/SearchPages.jsx
@@ -1,7 +1,35 @@
+import { HeroCard } from "../components/index";
+
 export const SearchPages = () => {
   return (
     <>
-      <h1>Search Pages</h1>
+      <h1>Search </h1>
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+
+          <form>
+            <input
+              type="text"
+              placeholder="Search a hero "
+              className="form-control"
+              name="searchText "
+              autoComplete="off"
+            />
+            <button className="btn btn-outline-primary mt-1">Search</button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Result</h4>
+          <hr />
+          <div className="alert alert-primary">Search a hero</div>
+          <div className="alert alert-danger">there no results</div>
+
+          {/* <HeroCard /> */}
+        </div>
+      </div>
     </>
   );
 };
