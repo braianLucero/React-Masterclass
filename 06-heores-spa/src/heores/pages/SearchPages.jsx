9c0f93a1fb45027b1ed05da72e62348c51c9052d
braian <brasanluc123@gmail.com>
2024-03-20 09:32:55 -0300

    showing list of heroes

--- 06-heores-spa/src/heores/pages/SearchPages.jsx
+++ 06-heores-spa/src/heores/pages/SearchPages.jsx
@@ -1,6 +1,27 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components/index";
+import { getHeroesByName } from "../helpers";
 
 export const SearchPages = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { q = "" } = queryString.parse(location.search);
+
+  const heroes = getHeroesByName(q);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: q,
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().length <= 1) return;
+
+    navigate(`?q=${searchText}`);
+  };
+
   return (
     <>
       <h1>Search </h1>
@@ -9,13 +30,15 @@ export const SearchPages = () => {
           <h4>Searching</h4>
           <hr />
 
-          <form>
+          <form onSubmit={onSearchSubmit}>
             <input
               type="text"
               placeholder="Search a hero "
               className="form-control"
-              name="searchText "
+              name="searchText"
               autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
             />
             <button className="btn btn-outline-primary mt-1">Search</button>
           </form>
@@ -25,9 +48,12 @@ export const SearchPages = () => {
           <h4>Result</h4>
           <hr />
           <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">there no results</div>
-
-          {/* <HeroCard /> */}
+          <div className="alert alert-danger">
+            there no results <b>{q}</b>
+          </div>
+          {heroes.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
