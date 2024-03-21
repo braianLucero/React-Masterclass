2470b9e32b528c57b2199c24edcc88d46f0c8e4e
braian <brasanluc123@gmail.com>
2024-03-21 13:03:14 -0300

    user logout

--- 06-heores-spa/src/auth/context/AuthProvider.jsx
+++ 06-heores-spa/src/auth/context/AuthProvider.jsx
@@ -7,17 +7,24 @@ const initialState = {
   logged: false,
 };
 
+const init = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  return {
+    logged: !!user,
+    user: user,
+  };
+};
+
 export const AuthProvider = ({ children }) => {
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, initialState, init);
 
   const login = (name = "") => {
+    const user = { id: "abc ", name };
     const action = {
       type: types.login,
-      payload: {
-        id: "abc",
-        name: name,
-      },
+      payload: user,
     };
+    localStorage.setItem("user", JSON.stringify(user));
     dispatch(action);
   };
   return (
