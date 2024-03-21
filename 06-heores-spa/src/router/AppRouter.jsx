f7d1c576d0805a68c464f6ab437db7a53da19ada
braian <brasanluc123@gmail.com>
2024-03-21 16:50:58 -0300

    developing public routes

--- 06-heores-spa/src/router/AppRouter.jsx
+++ 06-heores-spa/src/router/AppRouter.jsx
@@ -3,12 +3,23 @@ import { Route, Routes } from "react-router-dom";
 import { HeroesRoutes } from "../heores";
 import { LoginPages } from "../auth";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPages />} />
+        <Route
+          path="login/*"
+          element={
+            <PublicRoute>
+              <Routes>
+                <Route path="/*" element={<LoginPages />} />
+              </Routes>
+            </PublicRoute>
+          }
+        />
+
         <Route
           path="/*"
           element={
@@ -17,6 +28,7 @@ export const AppRouter = () => {
             </PrivateRoute>
           }
         />
+        {/* <Route path="login" element={<LoginPages />} /> */}
         {/* <Route path="/*" element={<HeroesRoutes />} /> */}
       </Routes>
     </>
