6cd0c2ac0ddcfdd5e72b0188dcf2e8b3f2856745
braian <brasanluc123@gmail.com>
2024-03-21 16:04:25 -0300

    private routes

--- 06-heores-spa/src/router/AppRouter.jsx
+++ 06-heores-spa/src/router/AppRouter.jsx
@@ -2,13 +2,22 @@ import { Route, Routes } from "react-router-dom";
 
 import { HeroesRoutes } from "../heores";
 import { LoginPages } from "../auth";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path="login" element={<LoginPages />} />
-        <Route path="/*" element={<HeroesRoutes />} />
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
+        {/* <Route path="/*" element={<HeroesRoutes />} /> */}
       </Routes>
     </>
   );
