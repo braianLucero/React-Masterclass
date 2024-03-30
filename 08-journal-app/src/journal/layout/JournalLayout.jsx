65caa5a1589552ba7640f782fcdad6a85eaa41fe
braian <brasanluc123@gmail.com>
2024-03-30 14:17:34 -0300

    creating the NavBar

--- 08-journal-app/src/journal/layout/JournalLayout.jsx
+++ 08-journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,9 +1,10 @@
 import { Box } from "@mui/system";
+import { Navbar } from "../components";
 export const JournalLayout = ({ children }) => {
   const drawerWidth = 240;
   return (
     <Box sx={{ display: "flex" }}>
-      {/* Navbar  drawerWidth*/}
+      <Navbar drawerWidth={drawerWidth} />
       {/* Sidebar  drawerWidth */}
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {/* Toolbar*/}
