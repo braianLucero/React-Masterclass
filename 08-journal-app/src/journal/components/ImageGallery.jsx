9a68d3f0ee75e8e4ca5c2b6ae7afbcb528649da6
braian <brasanluc123@gmail.com>
2024-04-01 18:32:55 -0300

    bringing images and saving in a data js folder

--- 08-journal-app/src/journal/components/ImageGallery.jsx
+++ 08-journal-app/src/journal/components/ImageGallery.jsx
@@ -1,3 +1,19 @@
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import { itemData } from "../data/img";
 export const ImageGallery = () => {
-  return <div>ImageGallery</div>;
+  return (
+    <ImageList sx={{ width: "100%" }} cols={3} rowHeight={164}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
 };
---
+++ 08-journal-app/src/journal/data/img.js
@@ -0,0 +1,50 @@
+export const itemData = [
+  {
+    img: "https://img.freepik.com/foto-gratis/pintura-lago-montana-montana-al-fondo_188544-9126.jpg?w=740&t=st=1712006505~exp=1712007105~hmac=4d395d71e9b19e97e40bc6e7b749e0d988a77aa802d4465813ad03aa7a3921bd",
+    title: "Breakfast",
+  },
+  {
+    img: "https://img.freepik.com/foto-gratis/fiesta-divertida-dj_23-2151108197.jpg?w=740&t=st=1712006923~exp=1712007523~hmac=ae70eaf69343a77540e329c7868e6d35906632fe1fd8b8c0f0c98090a0a96c96",
+    title: "Burger",
+  },
+  {
+    img: "https://img.freepik.com/foto-gratis/retrato-jugador-tenis-atletico_23-2151190026.jpg?w=826&t=st=1712007041~exp=1712007641~hmac=2f8bc44c7b417a0782467930fece5e0113c90b2ac56a5bd386678bcfaf97503f",
+    title: "Camera",
+  },
+  {
+    img: "https://img.freepik.com/foto-gratis/fiesta-divertida-dj_23-2151108168.jpg?w=740&t=st=1712006964~exp=1712007564~hmac=73373b841f4a8bb5b1abc32767bac9efb2317d4af28b1d3969c5682d852c71fe",
+    title: "Coffee",
+  },
+  {
+    img: "https://img.freepik.com/foto-gratis/fiesta-divertida-dj_23-2151108172.jpg?t=st=1712006964~exp=1712007564~hmac=9a2b610000ace65c2ddde79335601a352cba92aa99cb94bef067fb8c830deb6b",
+    title: "Hats",
+  },
+  {
+    img: "https://img.freepik.com/foto-gratis/representacion-3d-fondo-textura-hexagonal_23-2150796421.jpg?w=740&t=st=1712006712~exp=1712007312~hmac=affb552d6f1bae7085ad7dc01f24d9e476c8dee3a021c8b24563b318ad1415c5",
+    title: "Honey",
+  },
+  {
+    img: "https://img.freepik.com/foto-gratis/increiblemente-hermoso-paisaje-playa-arena-mar-impresionantes-nubes-cielo_181624-18834.jpg?w=740&t=st=1712006775~exp=1712007375~hmac=0a534efabca5cce134c0cc1fd9af80839ce07d79a6591829f7c2ec6b442dedbc",
+    title: "Basketball",
+  },
+  {
+    img: "https://img.freepik.com/foto-gratis/belleza-otonal-abstracta-patron-venas-hoja-multicolor-generado-ia_188544-9871.jpg?w=740&t=st=1712006771~exp=1712007371~hmac=a1a17abeb8db07737e9104e26d851678e55002f9a1f037b1e471b1ce99dbab0b",
+    title: "Fern",
+  },
+  {
+    img: "https://img.freepik.com/vector-gratis/fondo-abstracto-3d-formas-corte-papel-oscuro_87202-1009.jpg?w=740&t=st=1712006725~exp=1712007325~hmac=4d7c2fa39ca5ab14c05ec19b86fec7aaff8b9d495044a1b6b7d15475cc9cb2a3",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://img.freepik.com/foto-gratis/belleza-tranquila-naturaleza-reflejada-aguas-tranquilas-ia-generativa_188544-12798.jpg?w=740&t=st=1712006835~exp=1712007435~hmac=32c15783d628f389d097d9fbedf8111d5af493c71772a95959f6b7d7448008a5",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://img.freepik.com/foto-gratis/vibrantes-alas-mariposa-muestran-belleza-naturaleza-patrones-multicolores-generados-inteligencia-artificial_188544-84270.jpg?w=740&t=st=1712006855~exp=1712007455~hmac=fad7ad1020c9e84f49f19bfb4608898e7fc75e414d8eb994886901b1e9556642",
+    title: "Sea star",
+  },
+  {
+    img: "https://img.freepik.com/foto-gratis/anuncio-alimentos-perritos-calientes_23-2151258114.jpg?w=740&t=st=1712007077~exp=1712007677~hmac=9812778477615247ad040a3e0515dfdc1f328ee3d85f406859573723a2fc27da",
+    title: "Bike",
+  },
+];
