554080815cd2f99ac6ec31ac9eb77aea75355488
braian <brasanluc123@gmail.com>
2024-04-03 15:59:41 -0300

    creating a pokemon slice with one initialization and two actions

---
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,14 @@
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>Pokemon App</h1>
+      <hr />
+
+      <ul>
+        <li>Hola </li>
+        <li>Buenos dias </li>
+        <li>Buenas noches </li>
+      </ul>
+    </>
+  );
+};
