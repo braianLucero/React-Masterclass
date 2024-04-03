73aa2c0ff7eb6c57bb9aac92b85158c4d5bf3725
braian <brasanluc123@gmail.com>
2024-04-03 19:36:40 -0300

    using axios to call our pokemons api

--- 09-toolkit-redux/src/store/slices/pokemon/thunks.js
+++ 09-toolkit-redux/src/store/slices/pokemon/thunks.js
@@ -1,10 +1,16 @@
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
+import { pokemoApi } from "../../../api/pokemonApi";
 
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
     // Todo : realizar peticion http
 
-    // dispatch(setPokemons());
+    // URL de la API
+    const { data } = await pokemoApi.get(
+      `pokemon?limit=10&offset=${page * 10}`
+    );
+
+    dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
 };
