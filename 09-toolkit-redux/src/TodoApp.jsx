9d10a2327d6121dd87f12dd2e7411df8b2469d25
braian <brasanluc123@gmail.com>
2024-04-04 19:35:34 -0300

    Rtk query

---
+++ 09-toolkit-redux/src/TodoApp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const TodoApp = () => {
+  return (
+    <>
+      <h1>Todos - RTK Query</h1>
+      <hr />
+      <h1>isLoading...</h1>
+
+      <pre>...</pre>
+
+      <button>Next todo</button>
+    </>
+  );
+};
--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import { Provider } from "react-redux";
 import "./index.css";
 import { store } from "./store";
-import { PokemonApp } from "./PokemonApp.jsx";
+// import { PokemonApp } from "./PokemonApp.jsx";
+import { TodoApp } from "./TodoApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 );
