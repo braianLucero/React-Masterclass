f9c7c299920133d481acf76c91e2506dc0e96970
braian <brasanluc123@gmail.com>
2024-04-05 05:05:15 -0300

    configuring my authSlice

---
+++ 08-journal-app/src/store/auth/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    status: "checking",
+    uid: null,
+    email: null,
+    displayName: null,
+    photoUrl: null,
+    errorMessage: null,
+  },
+  reducers: {
+    login: (state, actions) => {},
+    logout: (statem, payload) => {},
+    chekingCredentials: (state) => {},
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { login, logout, chekingCredentials } = authSlice.actions;
