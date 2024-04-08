0e38718dd40307d06bb128e62d88b8e8670651f9
braian <brasanluc123@gmail.com>
2024-04-08 15:09:28 -0300

    creating the registration form

--- 08-journal-app/src/auth/Pages/RegisterPage.jsx
+++ 08-journal-app/src/auth/Pages/RegisterPage.jsx
@@ -1,11 +1,25 @@
 import { Link as RouterLink } from "react-router-dom";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
-// import { Google } from "@mui/icons-material";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks/useForm";
+
+const formDate = {
+  email: "brasanluc123@gmail.com",
+  password: 123456,
+  displayName: "Braian Lucero",
+};
+
 export const RegisterPage = () => {
+  const { displayName, email, password, onInputChange, formState } =
+    useForm(formDate);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  };
   return (
     <AuthLayout title="Crear cuenta">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -13,6 +27,9 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Nombre Completo"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -22,6 +39,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="correo@google.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -31,12 +51,15 @@ export const RegisterPage = () => {
               type="password"
               placeholder="Contrasenia"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Crear Cuenta
               </Button>
             </Grid>
