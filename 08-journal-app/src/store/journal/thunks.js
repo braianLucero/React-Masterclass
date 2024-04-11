9cdc8d96e13e6a5b03995b8194807d673d6fdd5c
braian <brasanluc123@gmail.com>
2024-04-11 11:25:29 -0300

    creating the new notes for cloud Firestore

--- 08-journal-app/src/store/journal/thunks.js
+++ 08-journal-app/src/store/journal/thunks.js
@@ -1,28 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
-export const journalSlice = createSlice({
-  name: "journal",
-  initialState: {
-    isSaving: true,
-    messageSaved: "",
-    notes: [],
-    activeNote: null,
-  },
-  reducers: {
-    addNewEmtyNote: (state, action) => {},
-    setActiveNote: (state, action) => {},
-    setNotes: (state, action) => {},
-    setSaving: (state) => {},
-    updateNote: (state, action) => {},
-    deleteNodeById: (state, action) => {},
-  },
-});
-
-// Action creators are generated for each case reducer function
-export const {
-  addNewEmtyNote, // esta action es para cuando aprete el boton del + , pueda crear otra entrada
-  setActiveNote, // esta action es para saber cual es la nota activa cuando hago click (enero,febrero , marzo ,abril .etc)
-  setNotes, // es para cargar las notas , cuando la tengo leidas de algun lugar
-  setSaving, // esta action es para cuando estoy grabando las notas
-  updateNote, // esta action es para actualizar una nota
-  deleteNodeById, // esta action es para eliminarla de nuestro listado
-} = journalSlice.actions;
+export const inicioDeNuevoNota = () => {
+  return async (dispatch) => {
+    // uid , para saber yo como quiero almacenar mi informacion en mi base de datos
+    const nuevaNota = {
+      title: "",
+      body: "",
+      date: new Date().getTime(),
+    };
+  };
+};
