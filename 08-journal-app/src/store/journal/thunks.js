85b77e73a18247dc6e5e8e58ed9189df34cb7648
braian <brasanluc123@gmail.com>
2024-04-11 12:36:30 -0300

    creating a new note and document so that the information is displayed in the firestore

--- 08-journal-app/src/store/journal/thunks.js
+++ 08-journal-app/src/store/journal/thunks.js
@@ -1,10 +1,23 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../firebase/config";
+
 export const inicioDeNuevoNota = () => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
     // uid , para saber yo como quiero almacenar mi informacion en mi base de datos
+
+    const { uid } = getState().auth;
+    console.log(uid);
     const nuevaNota = {
       title: "",
       body: "",
       date: new Date().getTime(),
     };
+    // ============================================== // es el uid del usuario autenticado
+    const nuevoDocumento = doc(collection(FirebaseDB, `${uid}/journal/notas`));
+    await setDoc(nuevoDocumento, nuevaNota);
+
+    // dispatch
+    // dispatch(nuevaNota)
+    // dispatch(activarNota)
   };
 };
--- 08-journal-app/src/store/store.js
+++ 08-journal-app/src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth";
-import { journalSlice } from "./journal/thunks";
+import { journalSlice } from "./journal/";
 
 export const store = configureStore({
   reducer: {
